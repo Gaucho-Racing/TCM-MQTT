8a9f5349b8f518c42f3613af9fc7e68172e5b578
austeyen <austinchan09128@gmail.com>
2025-01-26 02:01:34 -0800

    Initial skeleton successfully connects to nanomq broker

---
+++ MQTT.cpp
@@ -0,0 +1,39 @@
+#include <mqtt/async_client.h>
+#include <string> 
+#include <iostream>
+
+const std::string serverURI("tcp://localhost:1883");
+const std::string clientID("jetson_client");
+const std::string persistDir("./localstorage/");
+const std::string topic("ingest/ecu"); // test
+
+int main(){
+    mqtt::async_client client(serverURI,clientID,persistDir);
+
+    try{
+        std::cout << "Connecting..." << std::endl;
+        mqtt::connect_options options;
+        options.set_clean_session(true);
+        client.connect(options)->wait();
+        std::cout << "Success!" << std::endl;
+
+
+        // insert publisher and random data generator logic here
+        // use whatever topic to test
+
+        // in the future: implement local storage of generated random data(?)
+
+
+        client.disconnect()->wait();
+        std::cout << "Disconnected." << std::endl;
+
+ 
+    } catch(const mqtt::exception& e) {
+        std::cout << "error" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+
