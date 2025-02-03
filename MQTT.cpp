d7b4b0158a1fbc31c84c3777067c08b8a3657283
austeyen <austinchan09128@gmail.com>
2025-02-02 16:31:51 -0800

    refactor to loop forever

--- MQTT.cpp
+++ MQTT.cpp
@@ -1,13 +1,15 @@
 #include <mqtt/async_client.h>
 #include <string> 
 #include <iostream>
-#include <random>
-#include <vector>
+#include <random> // for random data
+#include <thread>  // for sleep
 
 const std::string serverURI("tcp://localhost:1883");
 const std::string clientID("jetson_client");
 const std::string persistDir("./localstorage/");
 const std::string topic("ingest/ecu"); // test
+const int interval(2); // publish interval
+
 
 int main(){
     mqtt::async_client client(serverURI,clientID,persistDir);
@@ -23,16 +25,18 @@ int main(){
         std::mt19937 generator(rd());
         std::uniform_int_distribution<uint8_t> dist(0, 255); // 1 byte = 8 bits = 2^8 = 256 
 
-        for (int i = 0; i < 69; i++) {
+        while(true){
             uint8_t arr[8];
             for (int j = 0; j < 8; j++) {
                 arr[j] = dist(generator); 
             }
             // [ add local storage logic ] 
-            client.publish(topic, arr, 8, 2, true)->wait(); // alternatively you can use message_ptr and make_message 
+            
+            client.publish(topic, arr, 8, 1, true)->wait(); // alternatively you can use message_ptr and make_message 
+            std::cout << "Published to " << topic << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(interval)); // publish interval
         }
-        std::cout << "Published data." << std::endl;
-
+        
         client.disconnect()->wait();
         std::cout << "Disconnected." << std::endl;
 
