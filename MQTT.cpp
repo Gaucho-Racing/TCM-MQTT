f48fa6cfe932f15341e0afdd435e43c82498125d
austeyen <austinchan09128@gmail.com>
2025-02-23 16:44:56 -0800

    make function, change table, yeah

--- MQTT.cpp
+++ MQTT.cpp
@@ -2,90 +2,86 @@
 #include <string> 
 #include <iostream>
 #include <random> // for random data
-#include <thread>  // for sleep
+// #include <thread>  // for sleep
 
 #include <sqlite3.h>
 
-const std::string serverURI("tcp://localhost:1883");
-const std::string clientID("jetson_client");
-const std::string persistDir("./localstorage/");
-const std::string topic("ingest/ecu"); // test
-const int interval(2); // publish interval
+using namespace std;
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-    int i;
-    for(i=0; i<argc; i++){
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
-}  // ripped from https://www.sqlite.org/quickstart.html
+const string serverURI("tcp://localhost:1883");
+const string clientID("jetson_client");
+const string persistDir("./persist/");
 
-std::string byteArrayToBinaryString(const uint8_t* arr) {
-    // converts byte array to 64 length string
-    std::string binaryStr;
-    for (int i = 0; i < 8; i++) { 
-        for (int j = 7; j >= 0; j--) { 
-            if (arr[i] & (1 << j)) {
-                binaryStr += '1';
-            } else {
-                binaryStr += '0';
-            }
-        }
+const string dbName("grdata.db");
+const string tableName = "gr25";
+
+mqtt::async_client client(serverURI,clientID,persistDir);
+sqlite3 *db; 
+// CREATE TABLE IF NOT EXISTS gr25 (timestamp INTEGER, topic TEXT, data BLOB, synced INTEGER);
+
+
+void connect(){
+    if(sqlite3_open(dbName.c_str(), &db) == SQLITE_OK){
+        cout << "Database connected!" << endl;
+    } else {
+        cerr << "Failed to connect to database." << endl;
+        return;
     }
-    return binaryStr;
+
+    try{
+        mqtt::connect_options options;
+        options.set_clean_session(false); 
+        client.connect(options)->wait(); 
+        cout << "MQTT Connected!" << endl;
+    } catch(const mqtt::exception& e) {
+        cerr << "MQTT error: " << e.what() << endl;
+        cerr << "Failed to connect to MQTT." << endl;
+        return;
+    }
+}
+
+void disconnect(){
+    sqlite3_close(db);
+    cout << "Disconnected from database." << endl;
+    client.disconnect()->wait();
+    cout << "Disconnected from MQTT." << endl;
+}
+
+void publishData(string nodeID, string messageID, const uint8_t arr[], int length){
+    string topic = "gr25/gr25-main/" + nodeID + "/" + messageID; 
+    int timestamp = 0;
+    bool synced = 0;
+
+    sqlite3_stmt* stmt;
+    string query = "INSERT INTO " + tableName + " (timestamp, topic, data, synced) VALUES (CAST((julianday('now') - 2440587.5)*86400000000 AS INTEGER), ?, ?, ?);";
+
+    sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr);
+    sqlite3_bind_text(stmt, 1, topic.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_blob(stmt, 2, arr, length, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 3, synced);
+    sqlite3_step(stmt); // add error checking? 
+    sqlite3_finalize(stmt);
+
+    try {
+        client.publish(topic, arr, length, 1, true)->wait();
+        cout << "Published to " << topic << endl;
+    } catch (const mqtt::exception &e) {
+        cerr << "MQTT error: " << e.what() << endl;
+    }
 }
 
 int main(){
-    mqtt::async_client client(serverURI,clientID,persistDir);
-    sqlite3 *db; // database connector pointer
+    connect();
 
-    sqlite3_open("test.db", &db);
-    // CREATE TABLE saved_data (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT CHECK (LENGTH(data) = 64) );
-    
-    char* error = nullptr;
-    try{
-        std::cout << "Connecting..." << std::endl;
-        mqtt::connect_options options;
-        options.set_clean_session(true);
-        client.connect(options)->wait();
-        std::cout << "Success!" << std::endl;
-
-        std::random_device rd;
-        std::mt19937 generator(rd());
-        std::uniform_int_distribution<uint8_t> dist(0, 255); // 1 byte = 8 bits = 2^8 = 256 
-
-        while(true){
-            uint8_t arr[8];
-            for (int j = 0; j < 8; j++) {
-                arr[j] = dist(generator); 
-            }
-            std::string binary_string = byteArrayToBinaryString(arr);
-
-            sqlite3_stmt* stmt;
-            const char* query = "INSERT INTO saved_data (data) VALUES (?);";
-            sqlite3_prepare_v2(db, query, -1, &stmt, nullptr);
-            sqlite3_bind_text(stmt, 1, binary_string.c_str(), -1, SQLITE_STATIC);
-            sqlite3_step(stmt);
-            sqlite3_finalize(stmt);
-            
-
-            client.publish(topic, arr, 8, 1, true)->wait(); // alternatively you can use message_ptr and make_message 
-            std::cout << "Published to " << topic << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(interval)); // publish interval
-        }
-        
-        client.disconnect()->wait();
-        std::cout << "Disconnected." << std::endl;
-
-    } catch(const mqtt::exception& e) {
-        std::cout << "error" << std::endl;
-        return -1;
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<uint8_t> dist(0, 255);
+    uint8_t arr[8];
+    for (int j = 0; j < 8; j++) {
+        arr[j] = dist(generator); 
     }
 
-    sqlite3_close(db);
-
+    publishData("ecu","0x01", arr, 8);
+    disconnect();
     return 0;
 }
-
-
