e05dc46f09b4593fd40d89ea126502ad7d94ef14
austeyen <austinchan09128@gmail.com>
2025-02-27 19:47:36 -0800

    timestamp added to message

--- MQTT.cpp
+++ MQTT.cpp
@@ -3,23 +3,27 @@
 #include <iostream>
 #include <random> // for random data
 // #include <thread>  // for sleep
-
+#include <chrono>
 #include <sqlite3.h>
+#include <vector>
 
 using namespace std;
 
 const string serverURI("tcp://localhost:1883");
 const string clientID("jetson_client");
 const string persistDir("./persist/");
-
 const string dbName("grdata.db");
-const string tableName = "gr25";
-
+const string tableName("gr25");
 mqtt::async_client client(serverURI,clientID,persistDir);
 sqlite3 *db; 
+
 // CREATE TABLE IF NOT EXISTS gr25 (timestamp INTEGER, topic TEXT, data BLOB, synced INTEGER);
 
-
+long long getTimestamp(){
+    auto now = std::chrono::system_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+    return ms;
+}
 void connect(){
     if(sqlite3_open(dbName.c_str(), &db) == SQLITE_OK){
         cout << "Database connected!" << endl;
@@ -48,22 +52,43 @@ void disconnect(){
 }
 
 void publishData(string nodeID, string messageID, const uint8_t arr[], int length){
+
     string topic = "gr25/gr25-main/" + nodeID + "/" + messageID; 
-    int timestamp = 0;
     bool synced = 0;
 
     sqlite3_stmt* stmt;
-    string query = "INSERT INTO " + tableName + " (timestamp, topic, data, synced) VALUES (CAST((julianday('now') - 2440587.5)*86400000000 AS INTEGER), ?, ?, ?);";
+    string query = "INSERT INTO " + tableName + " (timestamp, topic, data, synced) VALUES (?, ?, ?, ?);";
 
+    long long timestamp = getTimestamp();
+    
     sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr);
-    sqlite3_bind_text(stmt, 1, topic.c_str(), -1, SQLITE_STATIC);
-    sqlite3_bind_blob(stmt, 2, arr, length, SQLITE_STATIC);
-    sqlite3_bind_int(stmt, 3, synced);
+    sqlite3_bind_int64(stmt, 1, timestamp);
+    sqlite3_bind_text(stmt, 2, topic.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_blob(stmt, 3, arr, length, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 4, synced);
     sqlite3_step(stmt); // add error checking? 
     sqlite3_finalize(stmt);
 
+    int size = 10+length;
+    vector<uint8_t> m(size, 0);
+    for (int i = 7; i >= 0; i--) {
+        m[7-i] = timestamp >> ((i * (8))) & 0xFF;
+    }
+
+    for (int i = 10; i < size; i++) {
+        m[i] = arr[i-10];
+    }
+
+    // std::cout << "Byte representation: ";
+    // for (unsigned char byte : m) {
+    //     std::cout << std::hex << static_cast<int>(byte) << " ";  // Print in hex format
+    // }
+    // std::cout << std::endl; 
+
     try {
-        client.publish(topic, arr, length, 1, true)->wait();
+        // client.publish(topic, arr, length, 1, true)->wait();
+        client.publish(topic, m.data(), size, 1, true)->wait();
+
         cout << "Published to " << topic << endl;
     } catch (const mqtt::exception &e) {
         cerr << "MQTT error: " << e.what() << endl;
@@ -73,6 +98,7 @@ void publishData(string nodeID, string messageID, const uint8_t arr[], int lengt
 int main(){
     connect();
 
+    
     random_device rd;
     mt19937 generator(rd());
     uniform_int_distribution<uint8_t> dist(0, 255);
@@ -85,3 +111,4 @@ int main(){
     disconnect();
     return 0;
 }
+
